094c349af8480504ffdf093ae666fa5d5d6f90cb
Vinícius Basílio <vbasilio2019@gmail.com>
2023-10-20 19:15:30 -0300

    feature: sistema de rotas básico

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+require 'rotas.php';
+$request_uri = $_SERVER['REQUEST_URI'];
+
+$rotas = [
+    '/' => 'HomeController@index',
+];
+
+if(array_key_exists($request_uri, $rotas)){
+    list($controller, $action) = explode('@', $rotas[$request_uri]);
+    require 'controllers/' . $controller . '.php';
+    $controllerInstancia = new $controller();
+    $controllerInstancia->$action();
+}else{
+    header("HTTP/1.0 404 Not Found");
+    echo 'Página não encontrada.';
+}
